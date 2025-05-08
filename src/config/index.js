0cf69b9d609822e35b4bba8b537ec8f53cb09db2
Catherene <swismitha@yahoo.co.in>
2025-05-08 11:50:40 +0530

    Add config

---
+++ src/config/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    appId : process.env.APPID || '389801252',
+    cosmosdb : {
+        endpoint : process.env.DB_ENDPOINT,
+        key : process.env.DB_KEY,
+        databaseId : process.env.DB_ID || 'cosmicworks',
+        containerId : process.env.DB_CONTAINERID || 'customerreviews',
+        summcontainerId : process.env.DB_SUMMCONTAINERID || 'reviewsummary'
+    },
+    awsregion : process.env.REGION,
+    snstopic : process.env.SNSTOPIc,
+    ddb : {
+        reviewtable: process.env.DB_REVIEW_TABLE || 'customerreviews',
+        summarytable: process.env.DB_SUMM_TABLE || 'reviewsummary'
+    }
+}
